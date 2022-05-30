529a4842e5e7dbee799178adbd07495e762346d1
Shouvik11-b <81455826+Shouvik11-b@users.noreply.github.com>
2022-05-31 00:10:18 +0530

    updated sellersignupform

--- customuser/forms.py
+++ customuser/forms.py
@@ -24,8 +24,10 @@ class CustomerSignUpForm(UserCreationForm):
         return user
 
 class SellerSignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required') 
     class Meta(UserCreationForm.Meta):
         model = User
+        fields = ('username', 'email', 'password1', 'password2')
 
     def save(self, commit=True):
         user = super().save(commit=False)
